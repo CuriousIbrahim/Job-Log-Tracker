5bca36ba96f9e57857a02410c2385afac6fcacd6
Ibrahim Abdul Hameed <ibrahim74115@gmail.com>
2018-04-14 20:24:54 -0400

    Create Job model and its attributes

---
+++ src/main/java/model/Job.java
@@ -0,0 +1,14 @@
+package model;
+
+public class Job {
+
+    private int id;
+    private String title;
+    private String type;
+    private String company;
+    private String description;
+    private int timestampApplied;
+    private String location;
+
+
+}
