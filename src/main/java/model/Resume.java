a868db54903985b50d2489c79c7fbbf0598036cf
Ibrahim Abdul Hameed <ibrahim74115@gmail.com>
2018-04-14 20:38:15 -0400

    [Resume] Add constructor to initialize every attribute

--- src/main/java/model/Resume.java
+++ src/main/java/model/Resume.java
@@ -6,4 +6,9 @@ public class Resume {
     private byte[] resume;
     private String extension;
 
+    public Resume(int jobId, byte[] resume, String extension) {
+        this.jobId = jobId;
+        this.resume = resume;
+        this.extension = extension;
+    }
 }
