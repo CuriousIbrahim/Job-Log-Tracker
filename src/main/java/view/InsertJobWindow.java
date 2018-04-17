aed57423b2b3580e9bb4476578f12bd909adb80e
Ibrahim Abdul Hameed <ibrahim74115@gmail.com>
2018-04-17 18:35:26 -0400

    [InsertJobWindow] Create inner class OtherFilesList, add to UI

--- src/main/java/view/InsertJobWindow.java
+++ src/main/java/view/InsertJobWindow.java
@@ -12,6 +12,7 @@ import javafx.stage.Stage;
 import view.attribute.TextAttribute;
 import view.attribute.dropdown.DropdownAttribute;
 import view.attribute.file.FileAttribute;
+import view.attribute.special.OtherFileAttribute;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -34,7 +35,7 @@ public class InsertJobWindow extends Stage {
     private TextArea description;
 
     private List<DropdownAttribute> jobStatuses;
-    private List<FileAttribute> otherFile;
+    private OtherFilesList otherFilesList;
 
     public InsertJobWindow() {
 
@@ -59,7 +60,7 @@ public class InsertJobWindow extends Stage {
         description = new TextArea();
 
         jobStatuses = new ArrayList<>();
-        otherFile = new ArrayList<>();
+        otherFilesList = new OtherFilesList();
 
         // ============================================================
         // Add to mainGridPane
@@ -79,10 +80,11 @@ public class InsertJobWindow extends Stage {
         mainGridPane.add(description, 0, 4, 3, 2);
 
         // TODO: Display a List of DropDownAttribute and FileAttribute
+        mainGridPane.add(otherFilesList, 0, 7, 3, 4);
 
         HBox buttonBox = new HBox(submitBtn);
         buttonBox.setAlignment(Pos.CENTER);
-        mainGridPane.add(buttonBox, 1, 7);
+        mainGridPane.add(buttonBox, 1, 11);
 
         // ============================================================
 
@@ -93,6 +95,72 @@ public class InsertJobWindow extends Stage {
         Scene scene = new Scene(mainGridPane);
         setScene(scene);
 
+
+    }
+
+    class OtherFilesList extends GridPane {
+
+        private List<OtherFileAttribute> otherFileAttributes;
+        private Button addNewBtn;
+
+        private final int MAX = 3;
+
+        public OtherFilesList () {
+            otherFileAttributes = new ArrayList<>();
+
+            addNewBtn = new Button("Add New");
+
+            addNewBtn.setOnAction(event -> {
+                addItem();
+            });
+
+            load();
+
+            setHgap(10);
+            setVgap(10);
+
+        }
+
+        public void addItem() {
+
+            if (otherFileAttributes.size() >= MAX) {
+                return;
+            }
+
+            getChildren().clear();
+
+            OtherFileAttribute temp = new OtherFileAttribute();
+            temp.getXButton().setOnAction(event -> {
+                removeItem(temp.getUniqueId());
+            });
+
+            otherFileAttributes.add(0, temp);
+
+            load();
+
+        }
+
+        private void removeItem(int id) {
+            for (int i = 0; i < otherFileAttributes.size(); i++) {
+                if (id == otherFileAttributes.get(i).getUniqueId())
+                    otherFileAttributes.remove(i);
+            }
+
+            getChildren().clear();
+
+            load();
+        }
+
+        private void load() {
+            // Static
+            add(new Label("Other File"), 0, 0);
+            add(addNewBtn, 1, 0);
+
+            // Dynamic
+            for (int i = 0; i < otherFileAttributes.size(); i++)
+                add(otherFileAttributes.get(i), 0, i+2, 3, 1);
+
+        }
 
     }
 }
