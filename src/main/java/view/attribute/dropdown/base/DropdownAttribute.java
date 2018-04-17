2d2ab0d4f94160df59245df3c59d53c80ba2f3d0
Ibrahim Abdul Hameed <ibrahim74115@gmail.com>
2018-04-17 13:19:47 -0400

    Add DropdownAttribute class that extends BaseAttribute

---
+++ src/main/java/view/attribute/dropdown/base/DropdownAttribute.java
@@ -0,0 +1,51 @@
+package view.attribute.dropdown.base;
+
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.layout.GridPane;
+import view.attribute.base.BaseAttribute;
+
+import java.util.ArrayList;
+
+public class DropdownAttribute extends BaseAttribute {
+
+    protected ComboBox<String> options;
+    protected Button add;
+
+    protected DropdownAttribute (String labelName) {
+        super(labelName);
+
+        value = new GridPane();
+
+        options = new ComboBox<>();
+        add = new Button("Add Option");
+
+        ((GridPane)value).add(options, 1, 1);
+        ((GridPane)value).add(add, 2, 1);
+
+        ((GridPane)value).setHgap(5);
+
+        add(value, 1, 0);
+    }
+
+    protected void addOption(String option) {
+        this.options.getItems().add(option);
+    }
+
+    protected void setOptions(ArrayList<String> options) {
+        this.options.getItems().clear();
+
+        for (String o : options) {
+            addOption(o);
+        }
+    }
+
+    @Override
+    public String getValue() {
+        return options.getValue();
+    }
+
+    public void setInteract(boolean value) {
+        options.setDisable(value);
+    }
+}
