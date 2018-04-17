ed76464d0f20eb32f233f03251d28aaae9db349a
Ibrahim Abdul Hameed <ibrahim74115@gmail.com>
2018-04-17 15:38:53 -0400

    Create OtherFileAttribute class

---
+++ src/main/java/view/attribute/special/OtherFileAttribute.java
@@ -0,0 +1,31 @@
+package view.attribute.special;
+
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+import view.attribute.file.base.BaseFile;
+
+public class OtherFileAttribute extends GridPane {
+
+    private TextField name;
+    private BaseFile file;
+
+    public OtherFileAttribute() {
+
+        name = new TextField("Insert Name");
+        file = new BaseFile();
+
+        add(name, 0, 0);
+        add(file, 1, 0);
+
+    }
+
+    public OtherFileAttribute(String name, String filePath){
+        this();
+        this.name.setText(name);
+        file.setFilePath(filePath);
+    }
+
+    public String getName() {
+        return name.getText();
+    }
+}
