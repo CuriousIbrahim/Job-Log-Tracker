fd0f898f2fd747548dfaa7840224277679c6d7ac
Ibrahim Abdul Hameed <ibrahim74115@gmail.com>
2018-04-17 14:55:39 -0400

    [InsertJobWindow] Improve layout

--- src/main/java/view/InsertJobWindow.java
+++ src/main/java/view/InsertJobWindow.java
@@ -1,5 +1,6 @@
 package view;
 
+import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
@@ -41,6 +42,10 @@ public class InsertJobWindow extends Stage {
 
         mainGridPane = new GridPane();
 
+        // ============================================================
+        // Initialize variables
+        // ============================================================
+
         title = new TextAttribute("Title");
         type = new DropdownAttribute("Type");
 
@@ -56,37 +61,32 @@ public class InsertJobWindow extends Stage {
         jobStatuses = new ArrayList<>();
         otherFile = new ArrayList<>();
 
-        GridPane row1 = new GridPane();
-        row1.add(title, 0, 0);
-        row1.add(type, 1,0);
-        mainGridPane.add(row1, 0, 0);
+        // ============================================================
+        // Add to mainGridPane
+        // ============================================================
 
-        GridPane row2 = new GridPane();
-        row2.add(location, 0, 0);
-        row2.add(company, 1, 0);
-        row2.add(date, 2, 0);
-        mainGridPane.add(row2, 0, 1);
 
-        GridPane row3 = new GridPane();
-        row3.add(resume, 0, 0);
-        row3.add(coverLetter, 1, 0);
-        mainGridPane.add(row3, 0, 2);
+        mainGridPane.add(title, 0, 0);
+        mainGridPane.add(type, 2, 0);
 
-        HBox row4 = new HBox(new Label("Description"));
-        row4.setAlignment(Pos.CENTER_LEFT);
-        mainGridPane.add(row4, 0, 3);
+        mainGridPane.add(location, 0, 1);
+        mainGridPane.add(company, 1, 1);
+        mainGridPane.add(date, 2, 1);
 
-        HBox row5 = new HBox(description);
-        row5.setAlignment(Pos.CENTER);
-        mainGridPane.add(row5, 0, 4);
+        mainGridPane.add(new Label("Description"), 0, 2);
+        mainGridPane.add(description, 0, 3, 3, 2);
 
-        GridPane row6 = new GridPane();
         // TODO: Display a List of DropDownAttribute and FileAttribute
-        mainGridPane.add(row6, 0, 5);
 
-        HBox row7 = new HBox(submitBtn);
-        row7.setAlignment(Pos.CENTER);
-        mainGridPane.add(row7, 0, 6);
+        HBox buttonBox = new HBox(submitBtn);
+        buttonBox.setAlignment(Pos.CENTER);
+        mainGridPane.add(buttonBox, 1, 6);
+
+        // ============================================================
+
+        mainGridPane.setVgap(20);
+        mainGridPane.setHgap(20);
+        mainGridPane.setPadding(new Insets(25));
 
         Scene scene = new Scene(mainGridPane);
         setScene(scene);
