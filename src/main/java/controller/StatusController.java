19e732796ee0a4f94f8348ec6f56021542c7924c
Ibrahim Abdul Hameed <ibrahim74115@gmail.com>
2018-04-18 16:56:52 -0400

    [StatusController] Now resets database and sets options for ComboBoxes for JobStatusAttribute

--- src/main/java/controller/StatusController.java
+++ src/main/java/controller/StatusController.java
@@ -1,18 +1,22 @@
 package controller;
 
 import db.JobStatusDatabase;
-import db.dao.base.BaseDAO;
+import db.dao.StatusDAO;
 import view.InsertJobWindow;
 
 import java.sql.SQLException;
 
 public class StatusController {
 
-    BaseDAO statusDB;
+    StatusDAO statusDB;
 
     public StatusController(InsertJobWindow window) throws SQLException {
 
         statusDB = new JobStatusDatabase();
+
+        // Reset database and set the options for ComboBoxes in view
+        statusDB.clear();
+        window.setStatusOptions(statusDB.all());
 
     }
 }
