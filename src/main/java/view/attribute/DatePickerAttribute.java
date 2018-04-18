0e2eb74bc9f7e5f520676b6149d40ef12adf7b78
Ibrahim Abdul Hameed <ibrahim74115@gmail.com>
2018-04-17 21:01:32 -0400

    Create DatePickerAttribute class

---
+++ src/main/java/view/attribute/DatePickerAttribute.java
@@ -0,0 +1,23 @@
+package view.attribute;
+
+import javafx.scene.control.DatePicker;
+import view.attribute.base.BaseAttribute;
+
+import java.time.LocalDate;
+
+public class DatePickerAttribute extends BaseAttribute {
+
+    public DatePickerAttribute(String name, boolean now) {
+        super(name);
+
+        value = new DatePicker();
+
+        if(now)
+            ((DatePicker)value).setValue(LocalDate.now());
+    }
+
+    @Override
+    public String getValue() {
+        return null;
+    }
+}
