d0a5753482ac421d58721d35ba44f192536f0c96
Ibrahim Abdul Hameed <ibrahim74115@gmail.com>
2018-04-17 21:19:34 -0400

    Make JobStatusesList into its own class instead of an inner class

---
+++ src/main/java/view/attribute/list/JobStatusesList.java
@@ -0,0 +1,79 @@
+package view.attribute.list;
+
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.GridPane;
+import view.attribute.special.JobStatusAttribute;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class JobStatusesList extends GridPane {
+
+    private List<JobStatusAttribute> jobStatusAttributes;
+    private Button addNewBtn;
+
+    private final int MAX = 5;
+
+    public JobStatusesList () {
+        jobStatusAttributes = new ArrayList<>();
+
+        addNewBtn = new Button("Add New");
+
+        addNewBtn.setOnAction(event -> {
+            addItem();
+        });
+
+        load();
+
+        setHgap(10);
+        setVgap(10);
+
+    }
+
+    public void addItem() {
+
+        if (jobStatusAttributes.size() >= MAX) {
+            return;
+        }
+
+        getChildren().clear();
+
+        JobStatusAttribute temp = new JobStatusAttribute();
+        System.out.println(temp.getUniqueId());
+        temp.getXButton().setOnAction(event -> {
+            removeItem(temp.getUniqueId());
+        });
+
+        jobStatusAttributes.add(0, temp);
+
+        load();
+
+    }
+
+    private void removeItem(int id) {
+        for (int i = 0; i < jobStatusAttributes.size(); i++) {
+            if (id == jobStatusAttributes.get(i).getUniqueId())
+                jobStatusAttributes.remove(i);
+        }
+
+        getChildren().clear();
+
+        load();
+    }
+
+    private void load() {
+        // Static
+        add(new Label("Job Statuses"), 0, 0);
+        add(addNewBtn, 1, 0);
+
+        // Dynamic
+        for (int i = 0; i < jobStatusAttributes.size(); i++)
+            add(jobStatusAttributes.get(i), 0, i+2, 3, 1);
+
+    }
+
+    public List<JobStatusAttribute> getJobStatusAttributes() {
+        return jobStatusAttributes;
+    }
+}
