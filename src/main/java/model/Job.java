ae2fbafef166eb7983a6656dcf8222c88d129431
Ibrahim Abdul Hameed <ibrahim74115@gmail.com>
2018-04-17 21:58:30 -0400

    [Job] Add constructor that initializes every variable except id

--- src/main/java/model/Job.java
+++ src/main/java/model/Job.java
@@ -35,6 +35,20 @@ public class Job {
         this.otherFiles = otherFiles;
     }
 
+    public Job(String title, String type, String company, String description, int timestampApplied, String location,
+               List<JobStatus> jobStatuses, Resume resume, CoverLetter coverLetter, List<OtherFile> otherFiles) {
+        this.title = title;
+        this.type = type;
+        this.company = company;
+        this.description = description;
+        this.timestampApplied = timestampApplied;
+        this.location = location;
+        this.jobStatuses = jobStatuses;
+        this.resume = resume;
+        this.coverLetter = coverLetter;
+        this.otherFiles = otherFiles;
+    }
+
     public int getId() {
         return id;
     }
