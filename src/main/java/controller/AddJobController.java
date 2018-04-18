b4c8b795776bbc38e4ae77649304791ea8814b13
Ibrahim Abdul Hameed <ibrahim74115@gmail.com>
2018-04-17 20:31:38 -0400

    Create AddJobController class

---
+++ src/main/java/controller/AddJobController.java
@@ -0,0 +1,12 @@
+package controller;
+
+import model.Job;
+import view.InsertJobWindow;
+
+public class AddJobController {
+
+    public AddJobController(Job job, InsertJobWindow window) {
+
+    }
+
+}
