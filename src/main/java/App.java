2baa09d347878b7ad1662c2ea75c339dcb39f336
Ibrahim Abdul Hameed <ibrahim74115@gmail.com>
2018-04-17 22:12:48 -0400

    Create App class, this will be the main class

---
+++ src/main/java/App.java
@@ -0,0 +1,20 @@
+import javafx.application.Application;
+import javafx.stage.Stage;
+import view.InsertJobWindow;
+import controller.AddJobController;
+
+public class App extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        primaryStage = new InsertJobWindow();
+
+        new AddJobController((InsertJobWindow)primaryStage);
+
+        primaryStage.show();
+    }
+}
