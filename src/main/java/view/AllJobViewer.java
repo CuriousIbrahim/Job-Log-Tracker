07e3c8565d51acf7902dfc3f7c0535c892db4657
Ibrahim Abdul Hameed <ibrahim74115@gmail.com>
2018-04-20 00:36:15 -0400

    [AllJobViewer] Initialize allCards and allJobs

--- src/main/java/view/AllJobViewer.java
+++ src/main/java/view/AllJobViewer.java
@@ -3,16 +3,26 @@ package view;
 import javafx.scene.control.Button;
 import javafx.scene.layout.GridPane;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class AllJobViewer extends GridPane {
 
     private Button addNewJob;
 
+    private GridPane allCards;
+    private List<Card> allJobs;
+
     public AllJobViewer(List<Integer> jobId, List<String> jobTitle, List<String> jobType, List<String> company,
                         List<String> currentStatus) {
 
 
+        allCards = new GridPane();
+        allJobs = new ArrayList<>();
+
+
+
 
     }
+
 }
