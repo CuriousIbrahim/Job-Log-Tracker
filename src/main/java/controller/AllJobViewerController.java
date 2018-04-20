96e7c43ac42e5f71dc80f7100568bdcf5619b8c1
Ibrahim Abdul Hameed <ibrahim74115@gmail.com>
2018-04-20 03:26:10 -0400

    [AllJobViewerController] Get data from database and pass them to AllJobViewer

--- src/main/java/controller/AllJobViewerController.java
+++ src/main/java/controller/AllJobViewerController.java
@@ -3,9 +3,12 @@ package controller;
 import db.dao.JobDAO;
 import db.JobDatabase;
 
+import model.Job;
 import view.AllJobViewer;
 
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class AllJobViewerController {
 
@@ -14,6 +17,24 @@ public class AllJobViewerController {
     public AllJobViewerController(AllJobViewer view) throws SQLException {
 
         jobDB = new JobDatabase();
+
+        List<Integer> jobId = new ArrayList<>();
+        List<String> jobTitle = new ArrayList<>();
+        List<String> jobType = new ArrayList<>();
+        List<String> company = new ArrayList<>();
+        List<String> currentStatus = new ArrayList<>();
+
+        for (Job j : jobDB.all()) {
+            jobId.add(j.getId());
+            jobTitle.add(j.getTitle());
+            jobType.add(j.getType());
+            company.add(j.getCompany());
+            currentStatus.add(j.getCurrentJobStatus().getStatus());
+        }
+
+        for (int i = 0; i < jobId.size(); i++) {
+            view.addJob(jobId.get(i), jobTitle.get(i), jobType.get(i), company.get(i), currentStatus.get(i));
+        }
 
     }
 }
