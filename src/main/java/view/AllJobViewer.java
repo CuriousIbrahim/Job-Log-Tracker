cbf4f3f86ee17ee34c69d4cb1e7feaa8b42ad217
Ibrahim Abdul Hameed <ibrahim74115@gmail.com>
2018-04-20 00:29:10 -0400

    Create AllJobViewer class, extends GridPane

---
+++ src/main/java/view/AllJobViewer.java
@@ -0,0 +1,18 @@
+package view;
+
+import javafx.scene.layout.GridPane;
+
+import java.awt.*;
+import java.util.List;
+
+public class AllJobViewer extends GridPane {
+
+    private Button addNewJob;
+
+    public AllJobViewer(List<Integer> jobId, List<String> jobTitle, List<String> jobType, List<String> company,
+                        List<String> currentStatus) {
+
+
+
+    }
+}
