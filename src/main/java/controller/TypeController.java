6eccf94754ec318e5aff202e6417b2e93010f2b8
Ibrahim Abdul Hameed <ibrahim74115@gmail.com>
2018-04-21 00:28:38 -0400

    [TypeController] Retrieve job types from database and set them in view dropdown

--- src/main/java/controller/TypeController.java
+++ src/main/java/controller/TypeController.java
@@ -1,10 +1,22 @@
 package controller;
 
+import db.JobTypeDatabase;
+import db.dao.TypeDAO;
 import view.InsertJobWindow;
+
+import java.sql.SQLException;
 
 public class TypeController {
 
-    public TypeController(InsertJobWindow view) {
+    private TypeDAO typeDB;
+
+    public TypeController(InsertJobWindow view) throws SQLException {
+
+        typeDB = new JobTypeDatabase();
+
+        typeDB.clear();
+
+        view.setTypes(typeDB.all());
 
     }
 }
