cbeba3badf99c96e3e155eb06a5dc9b53d1d1f39
Ibrahim Abdul Hameed <ibrahim74115@gmail.com>
2018-04-30 22:16:27 -0400

    [Time] Add method that converts a timestamp to a LocalDate object

--- src/main/java/util/Time.java
+++ src/main/java/util/Time.java
@@ -1,5 +1,6 @@
 package util;
 
+import java.time.Instant;
 import java.time.LocalDate;
 import java.time.ZoneId;
 
@@ -7,6 +8,11 @@ public class Time {
 
     public static long localDateToUnixTimestamp(LocalDate date) {
         return date.atStartOfDay(ZoneId.systemDefault()).toEpochSecond();
+    }
+
+    // Answer from Holger on https://stackoverflow.com/questions/35183146/how-can-i-create-a-java-8-localdate-from-a-long-epoch-time-in-milliseconds
+    public static LocalDate unixTimestampToLocalDate(int timestamp){
+        return Instant.ofEpochMilli((long) timestamp).atZone(ZoneId.systemDefault()).toLocalDate();
     }
 
 }
