df2c58ae4492a9581c87b251d9c68c135944a24f
Ibrahim Abdul Hameed <ibrahim74115@gmail.com>
2018-05-09 23:53:29 -0400

    [BaseFile] Add open button that opens the file, is visible if filePath is not empty

--- src/main/java/view/attribute/file/base/BaseFile.java
+++ src/main/java/view/attribute/file/base/BaseFile.java
@@ -6,11 +6,14 @@ import javafx.scene.layout.GridPane;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 
+import java.awt.*;
 import java.io.File;
+import java.io.IOException;
 
 public class BaseFile extends GridPane {
 
     private Button selectFileBtn;
+    private Button openBtn;
     private TextField filePath;
 
     private FileChooser fileChooser;
@@ -20,9 +23,13 @@ public class BaseFile extends GridPane {
 
         filePath = new TextField();
         selectFileBtn = new Button("Select File");
+        openBtn = new Button("Open");
 
         add(filePath, 0, 0);
         add(selectFileBtn, 1, 0);
+        add(openBtn, 2, 0);
+
+        openBtn.setVisible(false);
 
         setHgap(5);
 
@@ -35,8 +42,17 @@ public class BaseFile extends GridPane {
 
             if (file != null) {
                 filePath.setText(file.getAbsolutePath());
+                openBtn.setVisible(true);
             }
 
+        });
+
+        openBtn.setOnAction(event -> {
+            try {
+                Desktop.getDesktop().open(new File(filePath.getText()));
+            } catch (IOException e) {
+                e.printStackTrace(System.out);
+            }
         });
 
         filePath.setEditable(false);
@@ -47,6 +63,8 @@ public class BaseFile extends GridPane {
         this();
 
         filePath.setText(path);
+
+        openBtn.setVisible(true);
     }
 
     public String getFilePath() {
@@ -55,5 +73,6 @@ public class BaseFile extends GridPane {
 
     public void setFilePath(String filePath) {
         this.filePath.setText(filePath);
+        openBtn.setVisible(true);
     }
 }
