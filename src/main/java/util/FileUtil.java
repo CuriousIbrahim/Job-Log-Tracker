356747a284c1d5f19df79d8954436de9b056c72a
Ibrahim Abdul Hameed <ibrahim74115@gmail.com>
2018-05-09 23:20:25 -0400

    [FileUtil] Add method that creates file in /temp dir

--- src/main/java/util/FileUtil.java
+++ src/main/java/util/FileUtil.java
@@ -1,6 +1,9 @@
 package util;
 
+import org.apache.commons.io.FileUtils;
+
 import java.io.File;
+import java.io.IOException;
 
 public class FileUtil {
 
@@ -8,6 +11,18 @@ public class FileUtil {
 
     private static void makeTempDir() {
         new File(TEMP_DIR).mkdir();
+    }
+
+    public static String addFile(String name, String extension, byte[] data) throws IOException {
+
+        makeTempDir();
+
+        File file = new File(TEMP_DIR, name + "." + extension);
+
+        FileUtils.writeByteArrayToFile(file, data);
+
+        return file.getAbsolutePath();
+
     }
 
 }
