f892e9cbf6dec2248bff17a6871041c7ab65c0a2
Ibrahim Abdul Hameed <ibrahim74115@gmail.com>
2018-05-09 23:15:32 -0400

    [FileUtil] Add method that creates a /temp dir

--- src/main/java/util/FileUtil.java
+++ src/main/java/util/FileUtil.java
@@ -1,7 +1,13 @@
 package util;
 
+import java.io.File;
+
 public class FileUtil {
 
+    private static String TEMP_DIR = "/temp";
 
+    private static void makeTempDir() {
+        new File(TEMP_DIR).mkdir();
+    }
 
 }
