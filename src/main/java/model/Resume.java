fea4e2fbd872156076cd816bc7e9e6310fca5233
Ibrahim Abdul Hameed <ibrahim74115@gmail.com>
2018-05-13 20:53:36 -0400

    [Resume] Override and implement equals method

--- src/main/java/model/Resume.java
+++ src/main/java/model/Resume.java
@@ -40,4 +40,39 @@ public class Resume {
     public void setExtension(String extension) {
         this.extension = extension;
     }
+
+    @Override
+    public boolean equals(Object obj) {
+        Resume resume = null;
+
+        if (obj instanceof Resume)
+            resume = (Resume) obj;
+        else
+            return false;
+
+        if (this.jobId != resume.jobId || !this.extension.equals(resume.extension))
+            return false;
+
+        boolean areBytesSame;
+
+        // Check if they are the same lengths first
+        if (this.resume.length == resume.resume.length) {
+
+            // Iterate each byte in each array and check if they are not similar
+            // Average & Worst = O(n)
+            for (int i = 0; i < this.resume.length; i++) {
+
+                if (this.resume[i] != resume.resume[i])
+                    return false;
+
+            }
+
+            areBytesSame = true;
+
+        } else
+            return false;
+
+        return ((this.jobId == resume.jobId) && (areBytesSame) && (this.extension.equals(resume.extension)));
+
+    }
 }
