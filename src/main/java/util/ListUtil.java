9a0ae58aa8846a089df84e56e59616c7c11ba826
Ibrahim Abdul Hameed <ibrahim74115@gmail.com>
2018-05-24 00:06:45 -0400

    [ListUtil] Add method that returns a String from List

--- src/main/java/util/ListUtil.java
+++ src/main/java/util/ListUtil.java
@@ -1,8 +1,21 @@
 package util;
 
+import java.util.List;
+
 public class ListUtil {
 
+    public static String listOfStringsToString(List<String> strings) {
 
+        String toReturn = "";
 
+        for (int i = 0; i < strings.size(); i++) {
+            toReturn += strings.get(i);
+            if (i != strings.size() - 1) {
+                toReturn += ", ";
+            }
+        }
+
+        return toReturn;
+    }
 
 }
