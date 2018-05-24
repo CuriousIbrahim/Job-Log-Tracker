5685e279bc1f63c6226ef8fc5cbdcc3d9b943bc2
Ibrahim Abdul Hameed <ibrahim74115@gmail.com>
2018-05-24 02:05:10 -0400

    [App] Use log4j to log "Starting application"

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,9 +1,13 @@
 import controller.AllJobViewerController;
 import javafx.application.Application;
 import javafx.stage.Stage;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import view.AllJobViewer;
 
 public class App extends Application {
+
+    private static final Logger logger = LogManager.getLogger(App.class);
 
     public static void main(String[] args) {
         launch(args);
@@ -11,6 +15,9 @@ public class App extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
+
+        logger.debug("Starting application");
+
         primaryStage = new AllJobViewer();
 
         new AllJobViewerController((AllJobViewer) primaryStage);
