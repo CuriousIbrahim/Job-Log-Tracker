88dc25a16f0d184f7af3c44b441daf4410fbe043
Ibrahim Abdul Hameed <ibrahim74115@gmail.com>
2018-06-02 22:29:15 -0400

    [CoreDatabase] Add documentation

--- src/main/java/db/core/CoreDatabase.java
+++ src/main/java/db/core/CoreDatabase.java
@@ -2,12 +2,27 @@ package db.core;
 
 import java.sql.*;
 
+/**
+ * Represents the basics for a SQLITE database, this includes making the database, running sql commands and simple
+ * queries
+ */
+
 public class CoreDatabase {
 
     private static final String SQLITE_URL = "jdbc:sqlite:";
     private static final String EXTENSION = ".db";
 
+    /**
+     * Is static a Connection to the database
+     */
+
     public static Connection conn;
+
+    /**
+     * Creates an SQLITE database
+     * @param dbName The name of the sqlite file <i>dbName</i>.db
+     * @throws SQLException
+     */
 
     public CoreDatabase(String dbName) throws SQLException  {
 
@@ -16,6 +31,12 @@ public class CoreDatabase {
         );
 
     }
+
+    /**
+     * Runs a query and returns the results
+     * @param sql SQL query command
+     * @return If an exception occurs, returns null, otherwise, returns the result from the query
+     */
 
     public ResultSet runQuery(String sql){
 
@@ -31,6 +52,12 @@ public class CoreDatabase {
         }
 
     }
+
+    /**
+     * Run an SQL command, like creating a table
+     * @param sql The SQL command
+     * @return If an exception occurs, returns false, otherwise, returns true
+     */
 
     public boolean runSql(String sql) {
 
