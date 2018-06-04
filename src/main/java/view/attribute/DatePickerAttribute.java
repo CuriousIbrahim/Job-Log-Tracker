c7505ef6c9d8cf599fe302cdb55b118a2eb7897b
Ibrahim Abdul Hameed <ibrahim74115@gmail.com>
2018-06-04 16:50:12 -0400

    [DatePickerAttribute] Add documentation

--- src/main/java/view/attribute/DatePickerAttribute.java
+++ src/main/java/view/attribute/DatePickerAttribute.java
@@ -5,7 +5,17 @@ import view.attribute.base.BaseAttribute;
 
 import java.time.LocalDate;
 
+/**
+ * An attribute that allows functionality to select dates and view them via DatePicker
+ */
+
 public class DatePickerAttribute extends BaseAttribute {
+
+    /**
+     * Constructs a DatePickerAttribute object, sets attribute label name and allows to set date to now by default
+     * @param name Label name of attribute
+     * @param now If true, will set date to now, if false, untested behaviour
+     */
 
     public DatePickerAttribute(String name, boolean now) {
         super(name);
@@ -18,10 +28,20 @@ public class DatePickerAttribute extends BaseAttribute {
         addValueNodeToPane();
     }
 
+    /**
+     * Returns attribute's value, the date
+     * @return Attribute's value, the date
+     */
+
     @Override
     public LocalDate getValue() {
         return ((DatePicker)value).getValue();
     }
+
+    /**
+     * Sets the value of the attribute
+     * @param o Value to place in view
+     */
 
     @Override
     public void setValue(Object o) {
