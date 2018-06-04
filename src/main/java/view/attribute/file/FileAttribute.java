bbc42e06500fcbf7abbaab669f3882c2ba004017
Ibrahim Abdul Hameed <ibrahim74115@gmail.com>
2018-06-04 16:45:34 -0400

    [FileAttribute] Add documentation

--- src/main/java/view/attribute/file/FileAttribute.java
+++ src/main/java/view/attribute/file/FileAttribute.java
@@ -3,7 +3,16 @@ package view.attribute.file;
 import view.attribute.base.BaseAttribute;
 import view.attribute.file.base.BaseFile;
 
+/**
+ * Represents the Attribute of a file, allows file input using a FileBrowser from BaseFile
+ */
+
 public class FileAttribute extends BaseAttribute {
+
+    /**
+     * Constructs FileAttribute object, sets label name of attribute
+     * @param labelName
+     */
 
     public FileAttribute (String labelName) {
         super(labelName);
@@ -14,10 +23,20 @@ public class FileAttribute extends BaseAttribute {
 
     }
 
+    /**
+     * Returns value of attribute
+     * @return Value of attribute
+     */
+
     @Override
     public String getValue() {
         return ((BaseFile)value).getFilePath();
     }
+
+    /**
+     * Sets value in view
+     * @param o Value to place in view
+     */
 
     @Override
     public void setValue(Object o) {
