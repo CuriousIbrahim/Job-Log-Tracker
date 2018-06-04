fcc203859370d5f56d9399b90ca0be12bf609119
Ibrahim Abdul Hameed <ibrahim74115@gmail.com>
2018-06-04 16:55:43 -0400

    [TextAttribute] Add documentation

--- src/main/java/view/attribute/TextAttribute.java
+++ src/main/java/view/attribute/TextAttribute.java
@@ -3,8 +3,16 @@ package view.attribute;
 import javafx.scene.control.TextField;
 import view.attribute.base.BaseAttribute;
 
+/**
+ * Represents the attribute in which the value is a simple String of text, inherits from BaseAttribute
+ */
+
 public class TextAttribute extends BaseAttribute {
 
+    /**
+     * Constructs TextAttribute object, sets attribute's label name
+     * @param labelName Label of attribute
+     */
 
     public TextAttribute (String labelName) {
         super(labelName);
@@ -15,6 +23,12 @@ public class TextAttribute extends BaseAttribute {
 
     }
 
+    /**
+     * Constructs TextAttribute object, sets attribute's label name and value name
+     * @param labelName Label of attribute
+     * @param valueName Value of attribute
+     */
+
     public TextAttribute(String labelName, String valueName) {
         super(labelName);
 
@@ -22,6 +36,13 @@ public class TextAttribute extends BaseAttribute {
 
         addValueNodeToPane();
     }
+
+    /**
+     * Constructs TextAttribute object, sets attribute's label name and value name; and sets the edibility of the value TextField
+     * @param labelName Label of attribute
+     * @param valueName Value of attribute
+     * @param isEditable If true, value TextField is edible, if false, value TextField is inedible
+     */
 
     public TextAttribute(String labelName, String valueName, boolean isEditable) {
         super(labelName);
@@ -32,10 +53,20 @@ public class TextAttribute extends BaseAttribute {
         addValueNodeToPane();
     }
 
+    /**
+     * Returns attribute's value
+     * @return Attribute's value
+     */
+
     @Override
     public String getValue() {
         return ((TextField)value).getText();
     }
+
+    /**
+     * Sets attribute's value and sets it in view
+     * @param o Value to place in view
+     */
 
     @Override
     public void setValue(Object o) {
@@ -49,8 +80,12 @@ public class TextAttribute extends BaseAttribute {
 
         ((TextField)value).setText(temp);
 
-
     }
+
+    /**
+     * Sets the edibility of the value TextField
+     * @param state If true, value TextField is edible, if false, value TextField is inedible
+     */
 
     public void setEditable(boolean state) {
         ((TextField)this.value).setEditable(state);
